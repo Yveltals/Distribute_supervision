3e47702e51935330ea9f98b710c0e89188ac8e82
Yveltals <1302540061@qq.com>
2022-11-19 22:55:53 +0800

    echarts

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -3,7 +3,7 @@
     <el-card style="user-select:none">
       <Row>
         <Col span="2" style="padding-left:10px;">
-          <el-image style="width:75px;height:75px" :src="top_icon">
+          <el-image style="width:75px;height:75px" :src="require('../assets/info_icon.png')">
           </el-image></Col>
         <Col span="22" style="padding-top:10px">
           <h1>基本信息</h1>
@@ -14,14 +14,17 @@
     <Divider style="margin: 10px;"/>
     <Row>
       <Col span="16">
-        <el-card style="height:700px">
+        <el-card style="height:700px;user-select:none">
           <h3 style="margin:10px">树结构图</h3>
+          <el-scrollbar style="height:600px">
+            <div class="echart" id="treeChart" style="height:800px;width:800px"></div>
+          </el-scrollbar>
         </el-card>
       </Col>
       <Col span="8">
         <el-card class="notice">
           <Row>
-            <Col><el-image class="image" :src="history_icon"></el-image></Col>
+            <Col><el-image class="image" :src="require('../assets/history.png')"></el-image></Col>
             <Col>
               <h3 style="margin-left:10px;line-height:40px;text-align:center;user-select:none">状态变更</h3>
             </Col>
@@ -45,7 +48,7 @@
         </el-card>
         <el-card class="info">
           <Row>
-            <Col><el-image class="image" :src="status_icon"></el-image></Col>
+            <Col><el-image class="image" :src="require('../assets/status.png')"></el-image></Col>
             <Col>
               <h3 style="margin-left:10px;line-height:40px;text-align:center;user-select:none">平台信息</h3>
             </Col>
@@ -53,7 +56,7 @@
           <div class="border" style="height:125px">
           <Row style="margin-top:8px;">
             <Col span="4">
-              <el-image class="image-small" :src="time_icon"></el-image></Col>
+              <el-image class="image-small" :src="require('../assets/time.png')"></el-image></Col>
             <Col >
               <h3>2022-11-12 15:32:57</h3>
               <p style="color:grey">统计截止时间</p>
@@ -61,13 +64,13 @@
           </Row>
           <Row style="margin-top:13px">
             <Col span="4">
-              <el-image class="image-small" :src="node_icon"></el-image></Col>
+              <el-image class="image-small" :src="require('../assets/node.png')"></el-image></Col>
             <Col span="6">
               <h3>13</h3>
               <p style="color:grey">机器数量</p>
             </Col>
             <Col span="4">
-              <el-image class="image-small" :src="account_icon"></el-image></Col>
+              <el-image class="image-small" :src="require('../assets/account.png')"></el-image></Col>
             <Col >
               <h3>1</h3>
               <p style="color:grey">用户数量</p>
@@ -80,54 +83,198 @@
   </div>
 </template>
 <script>
-  import * as echarts from "echarts";
-  export default {
-    name: 'Info',
-    data () {
-      return {
-        top_icon: require('../assets/info_icon.png'),
-        time_icon: require('../assets/time.png'),
-        account_icon: require('../assets/account.png'),
-        node_icon: require('../assets/node.png'),
-        status_icon: require('../assets/status.png'),
-        history_icon: require('../assets/history.png'),
-        activities: [{
-          content: '支持使用图标',
-          timestamp: '2018-04-12 20:46',
-          size: 'large',
-          type: 'primary',
-          icon: 'el-icon-more'
-        }, {
-          content: '支持自定义颜色',
-          timestamp: '2018-04-03 20:46',
-          color: '#0bbd87'
-        }, {
-          content: '支持自定义尺寸',
-          timestamp: '2018-04-03 20:46',
-          size: 'large'
-        }, {
-          content: '支持自定义尺寸',
-          timestamp: '2018-04-03 20:46',
-          size: 'large'
-        }, {
-          content: '支持自定义尺寸',
-          timestamp: '2018-04-03 20:46',
-          size: 'large'
-        }, {
-          content: '支持自定义尺寸',
-          timestamp: '2018-04-03 20:46',
-          size: 'large'
-        }],
-      }
-    },
-    created () {
-      
-    },
-    methods: {
-    
+import * as echarts from "echarts";
+import node_icon from '../assets/disk.png'
+export default {
+  name: 'Info',
+  data () {
+    return {
+      treeData: {
+        name: 'root',
+        image_url: node_icon,
+        class: ["rootNode"],
+        children: [
+          {
+            name: 'children1',
+            image_url: node_icon,
+            children: [
+              {
+                name: 'grandchild',
+                image_url: node_icon
+              },
+              {
+                name: 'grandchild2',
+                image_url: node_icon
+              },
+              {
+                name: 'grandchild3',
+                image_url: node_icon
+              },
+              {
+                name: 'grandchild2',
+                image_url: node_icon
+              },
+              {
+                name: 'grandchild3',
+                image_url: node_icon
+              }
+            ]
+          },
+          {
+            name: 'children2',
+            image_url: node_icon,
+            children: [
+              {
+                name: 'grandchild',
+                image_url: node_icon
+              },
+              {
+                name: 'grandchild2',
+                image_url: node_icon
+              },
+              {
+                name: 'grandchild3',
+                image_url: node_icon
+              },
+              {
+                name: 'grandchild2',
+                image_url: node_icon
+              },
+              {
+                name: 'grandchild3',
+                image_url: node_icon
+              }
+            ]
+          },
+        ]
+      },
+
+      activities: [{
+        content: '支持使用图标',
+        timestamp: '2018-04-12 20:46',
+        size: 'large',
+        type: 'primary',
+        icon: 'el-icon-more'
+      }, {
+        content: '支持自定义颜色',
+        timestamp: '2018-04-03 20:46',
+        color: '#0bbd87'
+      }, {
+        content: '支持自定义尺寸',
+        timestamp: '2018-04-03 20:46',
+        size: 'large'
+      }, {
+        content: '支持自定义尺寸',
+        timestamp: '2018-04-03 20:46',
+        size: 'large'
+      }, {
+        content: '支持自定义尺寸',
+        timestamp: '2018-04-03 20:46',
+        size: 'large'
+      }, {
+        content: '支持自定义尺寸',
+        timestamp: '2018-04-03 20:46',
+        size: 'large'
+      }],
     }
+  },
+  mounted () {
+    this.initTreeChart()
+  },
+  methods: {
+    initTreeChart(){
+      const data = {
+        name: 'flare',
+        children: [
+          {
+            name: 'display',
+            children: [
+              { name: 'DirtySprite', value: 8833 },
+              { name: 'LineSprite', value: 1732 },
+              { name: 'RectSprite', value: 3623 }
+            ]
+          },
+          {
+            name: 'scale',
+            children: [
+              { name: 'IScaleMap', value: 2105 },
+              { name: 'LinearScale', value: 1316 },
+              { name: 'LogScale', value: 3151 },
+              { name: 'OrdinalScale', value: 3770 },
+              { name: 'QuantileScale', value: 2435 },
+              { name: 'QuantitativeScale', value: 4839 },
+              { name: 'RootScale', value: 1756 },
+              { name: 'Scale', value: 4268 },
+              { name: 'QuantitativeScale', value: 4839 },
+              { name: 'RootScale', value: 1756 },
+              { name: 'Scale', value: 4268 },
+            ]
+          }
+        ]
+      };
+      let option = {
+        tooltip: {
+          trigger: 'item',
+          triggerOn: 'mousemove'
+        },
+        series: [
+          {
+            type: 'tree',
+            id: 0,
+            name: 'tree1',
+            data: [data],
+            top: '0%',
+            left: '10%',
+            bottom: '40px',
+            right: '20%',
+            symbol: 'image://'+node_icon,
+            symbolSize: 40,
+            edgeShape: 'polyline',
+            edgeForkPosition: '53%',
+            initialTreeDepth: 3,
+            lineStyle: {
+              width: 3
+            },
+            label: {
+              fontSize: 20,
+              lineHeight: 20,
+              backgroundColor: '#fff',
+              position: 'top',
+            },
+            leaves: {
+              label: {
+                position: 'right',
+                verticalAlign: 'middle',
+                align: 'left'
+              }
+            },
+            emphasis: {
+              focus: 'descendant'
+            },
+            expandAndCollapse: true,
+            animationDuration: 550,
+            animationDurationUpdate: 750
+          }
+        ]
+      };
+      const treeChart = echarts.init(document.getElementById("treeChart"));
+      treeChart.setOption(option);
+    },
   }
+}
 </script>
+<style>
+.node{
+  width: 120px !important;
+}
+.name{
+  margin-bottom: 0.5rem;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  line-height: 1.2;
+  color: rgb(90, 90, 90);
+}
+</style>
 <style scoped>
 #info {
   padding: 10px;
--- src/views/Node.vue
+++ src/views/Node.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="node">
+<div id="Node">
     <el-card style="user-select:none">
       <Row>
         <Col span="2" style="padding-left: 10px;">
@@ -12,15 +12,81 @@
       </Row>
     </el-card>
     <Divider style="margin: 10px;"/>
-    <el-card style="height:700px">
+    <el-card style="height:700px;padding:10px">
         <h3>节点ID:{{this.$store.state.nodeId}}</h3>
-        <div><img src="../assets/demo.png" alt="" class="img">  </div>
+        <Row>
+          <Col span="9">
+            <div class="echart" id="radarChart" style="width:450px;height:450px"></div>
+          </Col>
+          <Col>
+            <Row class="stride">
+              <Col span="4"><div class="echart" id="pieChart1" style="width:100px;height:100px"></div></Col>
+              <Col span="5" class="col">
+                <h3>4%</h3>
+                <h3 style="color:rgb(170, 170, 170)">CPU</h3>
+              </Col>
+              <Col span="5" class="col">
+                <h3>0.52 core</h3>
+                <h3 style="color:rgb(170, 170, 170)">已使用</h3>
+              </Col>
+              <Col span="5" class="col">
+                <h3>14 core</h3>
+                <h3 style="color:rgb(170, 170, 170)">总计</h3>
+              </Col>
+            </Row>
+            <Row class="stride">
+              <Col span="4"><div class="echart" id="pieChart2" style="width:100px;height:100px"></div></Col>
+              <Col span="5" class="col">
+                <h3>12%</h3>
+                <h3 style="color:rgb(170, 170, 170)">内存</h3>
+              </Col>
+              <Col span="5" class="col">
+                <h3>2.72 Gi</h3>
+                <h3 style="color:rgb(170, 170, 170)">已使用</h3>
+              </Col>
+              <Col span="5" class="col">
+                <h3>22.94 Gi</h3>
+                <h3 style="color:rgb(170, 170, 170)">总计</h3>
+              </Col>
+            </Row>   
+            <Row class="stride">
+              <Col span="4"><div class="echart" id="pieChart3" style="width:100px;height:100px"></div></Col>
+              <Col span="5" class="col">
+                <h3>26%</h3>
+                <h3 style="color:rgb(170, 170, 170)">容器组</h3>
+              </Col>
+              <Col span="5" class="col">
+                <h3>29</h3>
+                <h3 style="color:rgb(170, 170, 170)">已使用</h3>
+              </Col>
+              <Col span="5" class="col">
+                <h3>110</h3>
+                <h3 style="color:rgb(170, 170, 170)">总计</h3>
+              </Col>
+            </Row>   
+            <Row class="stride">
+              <Col span="4"><div class="echart" id="pieChart4" style="width:100px;height:100px"></div></Col>
+              <Col span="5" class="col">
+                <h3>21%</h3>
+                <h3 style="color:rgb(170, 170, 170)">本地存储</h3>
+              </Col>
+              <Col span="5" class="col">
+                <h3>9.11 GB</h3>
+                <h3 style="color:rgb(170, 170, 170)">已使用</h3>
+              </Col>
+              <Col span="5" class="col">
+                <h3>42.93 GB</h3>
+                <h3 style="color:rgb(170, 170, 170)">总计</h3>
+              </Col>
+            </Row>         
+          </Col>
+        </Row>
         <Button @click="backToList" class="back_button">返回</Button>  
     </el-card>
 </div>
 </template>
 <script>
-
+import * as echarts from "echarts";
 export default {
     name: 'Node',
     data () {
@@ -29,18 +95,289 @@ export default {
     }
     },
     created () {
-    
+        
+    },
+    mounted() {
+        this.initPieChart1()
+        this.initPieChart2()
+        this.initPieChart3()
+        this.initPieChart4()
+        this.initRadarChart() 
     },
     methods: {
         backToList(){
             this.$router.replace("/admins/list")
         },
+
+        initRadarChart() {
+          const option = {
+            tooltip: {
+              show: false,
+            },
+            legend: {
+              show: false,
+            },
+            radar: {
+              shape: 'circle',
+              indicator: [
+                { name: 'CPU', max: 6500 },
+                { name: '本地存储', max: 16000 },
+                { name: '内存', max: 30000 },
+                { name: '容器组', max: 38000 },
+              ],
+              splitArea: {
+                areaStyle: {
+                  color: ['#f9fcff'],
+                }
+              },
+              axisLine: {
+                lineStyle: {
+                  color: '#D3D3D3'
+                }
+              },
+              splitLine: {
+                lineStyle: {
+                  width: 2,
+                  color: '#f3f3f3'
+                }
+              }
+            },
+            series: [
+              {
+                type: 'radar',
+                data: [
+                  {
+                    value: [4200, 3000, 20000, 35000, 50000, 18000],
+                    name: 'Allocated Budget',
+                    symbolSize: 2,
+                    areaStyle: {
+                      color: 'rgba(100, 100, 100, 0.6)'
+                    },
+                    lineStyle: {
+                      width: 2,
+                      color: 'rgba(100, 100, 100, 0.7)'
+                    }
+                  },
+                ],
+              }
+            ]
+          };
+          const pieChart = echarts.init(document.getElementById("radarChart"));// 图标初始化
+          pieChart.setOption(option);// 渲染页面
+          //随着屏幕大小调节图表
+          window.addEventListener("resize", () => {
+            pieChart.resize();
+          });
+        },
+        initPieChart1() {
+          // let chartData = [];
+          // for (let key in this.emotionsData) {
+          //   var obj = {};
+          //   obj.name = scoreEmoMap[key];
+          //   obj.value = this.emotionsData[key];
+          //   chartData.push(obj);
+          // }
+          const option = {
+            tooltip: {
+              show: false,
+            },
+            legend: {
+              show: false,
+            },
+            series: [
+              {
+                type: 'pie',
+                label:{
+                    show: false,
+                },
+                radius: ['40%', '70%'],
+                data: [
+                  { value: 30, name: 'CPU已使用' },
+                  { value: 70, name: 'CPU未使用' },
+                ],
+                itemStyle: {
+                  normal: {
+                    color: function (colors) {
+                      var colorList = [
+                        '#0099CC',
+                        '#daebfa',
+                      ];
+                      return colorList[colors.dataIndex];
+                    }
+                  },
+                },
+                emphasis: {
+                  itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+                }
+              }
+            ]
+          };
+          const pieChart = echarts.init(document.getElementById("pieChart1"));
+          pieChart.setOption(option);
+        },
+        initPieChart2() {
+          const option = {
+            tooltip: {
+              show: false,
+            },
+            legend: {
+              show: false,
+            },
+            series: [
+              {
+                type: 'pie',
+                label:{
+                    show: false,
+                },
+                radius: ['40%', '70%'],
+                data: [
+                  { value: 30, name: 'CPU已使用' },
+                  { value: 70, name: 'CPU未使用' },
+                ],
+                itemStyle: {
+                  normal: {
+                    color: function (colors) {
+                      var colorList = [
+                        '#0099CC',
+                        '#daebfa',
+                      ];
+                      return colorList[colors.dataIndex];
+                    }
+                  },
+                },
+                emphasis: {
+                  itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+                }
+              }
+            ]
+          };
+          const pieChart = echarts.init(document.getElementById("pieChart2"));// 图标初始化
+          pieChart.setOption(option);// 渲染页面
+          //随着屏幕大小调节图表
+          window.addEventListener("resize", () => {
+            pieChart.resize();
+          });
+        },
+        initPieChart3() {
+          const option = {
+            tooltip: {
+              show: false,
+            },
+            legend: {
+              show: false,
+            },
+            series: [
+              {
+                type: 'pie',
+                label:{
+                    show: false,
+                },
+                radius: ['40%', '70%'],
+                data: [
+                  { value: 30, name: 'CPU已使用' },
+                  { value: 70, name: 'CPU未使用' },
+                ],
+                itemStyle: {
+                  normal: {
+                    color: function (colors) {
+                      var colorList = [
+                        '#0099CC',
+                        '#daebfa',
+                      ];
+                      return colorList[colors.dataIndex];
+                    }
+                  },
+                },
+                emphasis: {
+                  itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+                }
+              }
+            ]
+          };
+          const pieChart = echarts.init(document.getElementById("pieChart3"));// 图标初始化
+          pieChart.setOption(option);// 渲染页面
+          window.addEventListener("resize", () => {
+            pieChart.resize();
+          });
+        },
+        initPieChart4() {
+          const option = {
+            tooltip: {
+              show: false,
+            },
+            legend: {
+              show: false,
+            },
+            series: [
+              {
+                type: 'pie',
+                label:{
+                    show: false,
+                },
+                radius: ['40%', '70%'],
+                data: [
+                  { value: 30, name: 'CPU已使用' },
+                  { value: 70, name: 'CPU未使用' },
+                ],
+                itemStyle: {
+                  normal: {
+                    color: function (colors) {
+                      var colorList = [
+                        '#0099CC',
+                        '#daebfa',
+                      ];
+                      return colorList[colors.dataIndex];
+                    }
+                  },
+                },
+                emphasis: {
+                  itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+                }
+              }
+            ]
+          };
+          const pieChart = echarts.init(document.getElementById("pieChart4"));// 图标初始化
+          pieChart.setOption(option);// 渲染页面
+          //随着屏幕大小调节图表
+          window.addEventListener("resize", () => {
+            pieChart.resize();
+          });
+        },
     }
 }
 </script>
 <style scoped>
-#node{
+#Node{
     padding: 10px;
+}
+.stride{
+  margin: 10px 0px 10px;
+  width: 700px;
+  background: #f9fcff;
+}
+.col{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.test{
+  color: #daebfa
 }
 .back_button{
     position:absolute;
